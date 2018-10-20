223a8c5843354b472c4271c345f979267520b95d
Anton Kuznetsov <isnifer@gmail.com>
2018-10-20 20:55:30 +0300

    Add config

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    'eslint-config-airbnb-base',
+    'plugin:vue/essential',
+  ],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  plugins: [
+    'vue',
+  ],
+}
