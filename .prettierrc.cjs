81776d0335636d0ec9e6b0a3cabb8148dec02ced
Anton Kuznetsov <isnifer@gmail.com>
2023-02-01 13:15:24 +0400

    []2.5.0] Moved to eslint@8, eslint-plugin-vue@9, eslint-config-airbnb-base@15

---
+++ .prettierrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: 'avoid',
+  overrides: [
+    {
+      files: '*.vue',
+      options: { parser: 'vue' },
+    },
+  ],
+}
